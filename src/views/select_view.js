d97191044652306763917049346a5d7451d4df8c
Gregor Cox <gregorcox@gmail.com>
2018-08-07 19:36:17 +0100

    adds methods to populate drop down menu with instruments

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -1,1 +1,28 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function(element){
+  this.element = element;
+};
+
+SelectView.prototype.bindEvents = function(){
+  PubSub.subscribe('Instrument:all-instruments-ready', (evt) => {
+    const allInstruments = evt.detail;
+    this.populate(allInstruments);
+  });
+
+  this.element.addEventListener('change', (evt) => {
+    const selectedIndex = evt.target.value;
+    PubSub.publish('SelectView:change', selectedIndex);
+  });
+};
+
+SelectView.prototype.populate = function(instrumentsData){
+  instrumentsData.forEach((instrument, index) => {
+    const option = document.createElement('option');
+    option.textContent = animal.name;
+    option.value = index;
+    this.element.appendChild(option);
+  })
+}
+
+module.exports = SelectView;
