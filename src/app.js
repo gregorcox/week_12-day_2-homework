9a2213b1c55b7cfc0a05e565559a8567ff7ebe90
Gregor Cox <gregorcox@gmail.com>
2018-08-07 21:33:40 +0100

    returns name and description of instrument

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const Instruments = require('./models/instrument_families.js');
 const SelectView = require('./views/select_view.js');
+const ResultView = require('./views/result_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const selectElement = document.querySelector('select#instrument-families');
@@ -8,14 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const instrumentsDropdown = new SelectView(selectElement);
   instrumentsDropdown.bindEvents();
 
-
-  // const infoDiv = document.querySelector('div#instrument-info')
-  // const instrumentInfoDisplay = new ResultView(infoDiv);
-  // instrumentInfoDisplay.bindEvents();
+  const infoDiv = document.querySelector('div#instrument-info')
+  const instrumentInfoDisplay = new ResultView(infoDiv);
+  instrumentInfoDisplay.bindEvents();
+  console.log(instrumentInfoDisplay);
 
   const instrumentDataSource = new Instruments();
   instrumentDataSource.bindEvents();
 
-  console.log(instrumentDataSource);
+
 
 });
--- src/views/result_view.js
+++ src/views/result_view.js
@@ -1,1 +1,28 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+
+const ResultView = function(container){
+  this.container = container;
+}
+
+ResultView.prototype.bindEvents = function(){
+  PubSub.subscribe('Instrument:selected-instrument-ready', (evt) => {
+    const instrument = evt.detail;
+    this.render(instrument);
+  });
+};
+
+ResultView.prototype.render = function(instrument){
+  const nameParagraph = document.createElement('p');
+  const descriptionParagraph = document.createElement('p');
+  const instrumentsParagraph = document.createElement('p');
+  nameParagraph.textContent = `${instrument.name}`;
+  descriptionParagraph.textContent = `${instrument.description}`;
+  instrumentsParagraph.textContent = `${instrument.instruments}`;
+  this.container.innerHTML = '';
+  this.container.appendChild(nameParagraph);
+  this.container.appendChild(descriptionParagraph);
+  this.container.appendChild(instrumentsParagraph);
+};
+
+module.exports = ResultView;
